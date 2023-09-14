94086395675b32ddfe58bc4b11975682070ad25b
stefanycarnielli <stefany.silva4@estudante.ifms.edu.br>
2023-09-14 08:14:29 -0400

    s

--- src/componentes/portfolio/Portfolio.tsx
+++ src/componentes/portfolio/Portfolio.tsx
@@ -7,9 +7,10 @@ function Portfolio(){
                        
 <div className="text-portfolio">
             <h1>Quem sou eu?</h1>
-            <p>Sou Stefany Carnielli, tenho 17 anos de idade e atualmente estou no sexto semestre em Informática para Internet no Instituto Federal do Mato Grosso do Sul. </p>
+         <p> Sou Stefany Carnielli, tenho 17 anos de idade, sou nativa de Naviraí - MS, onde resido atualmente e estou no sexto semestre em Informática para Internet no Instituto Federal do Mato Grosso do Sul. </p>
+         <p> Tenho grande interesse na área da tecnologia e planos de cursar Direito posteriormente.</p> 
+        <p> Sou uma pessoa proativa e colaborativa que valoriza o trabalho em equipe.</p>  
         </div>
-
             </div>
         
     )
