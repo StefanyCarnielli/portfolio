e2b16a7cafdc4b7377e80132ea17647802ac3fbe
stefanycarnielli <stefany.silva4@estudante.ifms.edu.br>
2023-09-20 13:08:45 -0400

    arrumando as imagens

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -6,13 +6,6 @@ import { useState } from 'react'
 import Projeto from './../projeto/Projeto'
 import './Main.css'
 
-<div className="foto_projeto">
-<div><img src={Informatica} alt=""></img></div>
-<div><img src= {Logica} alt=""></img></div>
-<div><img src={Robotica} alt=""></img></div>
-<div><img src={Arduino} alt=""></img></div>
-</div>
-
 type ProjetoType = {
     id:number,
     titulo:string,
@@ -30,26 +23,26 @@ export default function Main() {
             id:1,
             titulo:'Informática para internet',
             descricao:"O técnico em Informática para Internet desenvolve programas de computador e aplicativos para dispositivos móveis, utiliza ferramentas de desenvolvimento de sistemas para construir soluções que auxiliam o processo de criação de interfaces e aplicativos empregados no comércio e marketing eletrônicos.",
-            imagem:'src/assets/img/informatica.png'
+            imagem:Informatica
         },
         {
             id:2,
             titulo:'Lógica de programação com Arduino',
             descricao:'Conceitos e Sintaxe da Linguagem de Programação para Aplicações prática, como:  Imprimindo uma mensagem no LCD, Alterando a frequência com que o LED pisca, Semáforo de Carros e Pedestres, Termômetro, Piano, Alarme, Projeto Alarme Multipropósito e Portão Eletrônico',
-            imagem:'src/assets/img/logica.png'
+            imagem:Logica
         },
         {
             id:3,
             titulo:'Programação de Robótica Lego EV3',
             descricao:'Introdução ao Software Lego Mindstorm EV3  e suas principais funcionalidades. Entendendo sobre motores, sensores para detecção de obstáculos e comutação. Tratamento de Erros.  Personalizar Blocos. Estratégia de seguidor de linha.',
-            imagem:'src/assets/img/robotica.png'
+            imagem:Robotica
         },
 
         {
             id:4,
             titulo:'Introdução á lógica de programação Arduino',
             descricao:'Introdução ao Software Lego Mindstorm EV3  e suas principais funcionalidades. Entendendo sobre motores, sensores para detecção de obstáculos e comutação. Tratamento de Erros.  Personalizar Blocos. Estratégia de seguidor de linha.',
-            imagem:'src/assets/img/arduino.png'
+            imagem:Arduino
         }
     ]
 
